c35f5034277fbc33cbb5dad6d14a277fbc37e1f4
Travis Plunk <travis@ez13.net>
2015-04-26 21:34:50 -0700

    Refactor Phases code

--- PoshBuildTools/BuildTools.psm1
+++ PoshBuildTools/BuildTools.psm1
@@ -2,9 +2,12 @@
 
 Set-StrictMode -Version latest
 $webClient = New-Object 'System.Net.WebClient';
-$repoName = ${env:APPVEYOR_REPO_NAME}
-$branchName = $env:APPVEYOR_REPO_BRANCH
-$pullRequestTitle = ${env:APPVEYOR_PULL_REQUEST_TITLE}
+$global:appveyor_repoName = ${env:APPVEYOR_REPO_NAME}
+$global:appveyor_repoBranch = $env:APPVEYOR_REPO_BRANCH
+$global:appveyor_pullRequestTitle = ${env:APPVEYOR_PULL_REQUEST_TITLE}
+$script:moduleBuildCount = 0
+$script:failedTestsCount = 0
+$script:passedTestsCount = 0
 
 function Invoke-RunTest {
     param
@@ -24,6 +27,156 @@ function Invoke-RunTest {
     Write-Info 'Done running tests.'
     return $res
 }
+
+Function Invoke-AppveyorInstall
+{
+    param
+    (
+        [switch]
+        $installPester
+    )
+
+    Write-Info 'Starting Install stage...'
+    Write-Info "Repo: $global:appveyor_repoName"
+    Write-Info "Branch: $global:appveyor_repoBranch"
+    if($global:appveyor_pullRequestTitle)
+    {
+        Write-Info "Pull Request:  $global:appveyor_pullRequestTitle"    
+    }
+
+    if($installPester)
+    {
+        Install-NugetPackage -package pester
+    }
+    
+    Write-Info 'End Install stage.'
+}
+
+function Test-BuildInfoList
+{
+    param
+    (
+        $list
+    )
+    
+    $list | ForEach-Object {
+        if($_.pstypenames -inotcontains $buildInfoType)
+        {
+            throw "Must be an array of type $buildInfoType"
+        }
+    }
+}
+Function Invoke-AppveyorBuild
+{
+    param
+    (
+        [ValidateScript({ Test-BuildInfoList -list $_})]
+        [PsObject[]] $moduleInfoList
+    )
+    Write-Info 'Starting Build stage...'
+    mkdir -force .\out > $null
+    mkdir -force .\nuget > $null
+    mkdir -force .\examples > $null
+
+    foreach($moduleInfo in $moduleInfoList)
+    {
+        $ModuleName = $moduleInfo.ModuleName
+        $ModulePath = $moduleInfo.ModulePath
+        if(test-path $modulePath)
+        {
+            Update-ModuleVersion -modulePath $ModulePath -moduleName $moduleName
+            
+            Update-Nuspec -modulePath $ModulePath -moduleName $ModuleName
+
+            Write-Info 'Creating nuget package ...'
+            nuget pack "$modulePath\${ModuleName}.nuspec" -outputdirectory  .\nuget
+
+            Write-Info 'Creating module zip ...'
+            7z a -tzip ".\out\$ModuleName.zip" ".\$ModuleName\*.*"
+
+            $script:moduleBuildCount ++
+        }
+        else 
+        {
+            Write-Warning "Couldn't find module, $ModuleName at $ModulePath.."
+        }
+    }
+    Write-Info 'End Build Stage.'
+}
+Function Invoke-AppveyorFinish
+{
+    param
+    (
+        [ValidateScript({ Test-BuildInfoList -list $_})]
+        [PsObject[]] $moduleInfoList,
+        [int] $expectedModuleCount
+    )
+    Write-Info 'Starting finish stage...'
+
+
+    if ($script:failedTestsCount -gt 0) 
+    { 
+        throw "$($script:failedTestsCount) tests failed."
+    } 
+    elseif($script:passedTestsCount -eq 0)
+    {
+        throw 'no tests passed'
+    }
+    elseif($script:moduleBuildCount -ne $expectedModuleCount)
+    {
+        throw "built ${script:moduleBuildCount} modules, but expected ${expectedModuleCount}"
+    } 
+    else 
+    {       
+        if($global:appveyor_repoBranch -ieq 'master' -and [string]::IsNullOrEmpty($global:appveyor_pullRequestTitle))
+        {
+        Get-ChildItem .\nuget | % { 
+                    Write-Info "Pushing nuget package $_.Name to Appveyor"
+                    Push-AppveyorArtifact $_.FullName
+            }
+        }
+        else 
+        {
+            Write-Info 'Skipping nuget package publishing because the build is not for the master branch or is a pull request.'
+        }
+    }
+    Get-ChildItem .\out | % { Push-AppveyorArtifact $_.FullName }
+    Write-Info 'End Finish Stage.'
+
+}
+Function Invoke-AppveyorTest
+{
+    param
+    (
+        [ValidateScript({ Test-BuildInfoList -list $_})]
+        [PsObject[]] $moduleInfoList
+    )
+    Write-Info 'Starting Test stage...'
+    # setup variables for the whole build process
+    #
+    #
+
+    foreach($moduleInfo in $moduleInfoList)
+    {
+        $ModuleName = $moduleInfo.ModuleName
+        $ModulePath = $moduleInfo.ModulePath
+        $ModulePath = $moduleInfo.ModulePath
+        if(test-path $modulePath)
+        {
+            $CodeCoverage = $moduleInfo.CodeCoverage
+            $tests = $moduleInfo.Tests
+            $tests | %{ 
+                $res = Invoke-RunTest -filePath $_ -CodeCoverage $CodeCoverage
+                $script:failedTestsCount += $res.FailedCount 
+                $script:passedTestsCount += $res.PassedCount 
+                $CodeCoverageTitle = 'Code Coverage {0:F1}%'  -f (100 * ($res.CodeCoverage.NumberOfCommandsExecuted /$res.CodeCoverage.NumberOfCommandsAnalyzed))
+                $res.CodeCoverage.MissedCommands | ConvertTo-FormattedHtml -title $CodeCoverageTitle | out-file .\out\CodeCoverage.html
+            }
+        }
+    }
+    Write-Info 'End Test Stage.'
+}
+
 
 function New-AppVeyorTestResult
 {
@@ -167,6 +320,7 @@ function Update-Nuspec
     
     Update-NuspecXml -nuspecXml $xml -nuspecPath $nuspecPath
 }
+$buildInfoType = 'PoshBuildTools.Build.ModuleInfo'
 
 function New-BuildModuleInfo
 {
@@ -190,7 +344,7 @@ function New-BuildModuleInfo
         Tests = $Tests
         }
     $moduleInfo.pstypenames.clear()
-    $moduleInfo.pstypenames.add('PoshBuildTools.Build.ModuleInfo')
+    $moduleInfo.pstypenames.add($buildInfoType)
     return $moduleInfo
 }
 function Update-NuspecXml
